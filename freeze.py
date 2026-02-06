6258f937de2b9c0a6aeb2e2089d405b1eea11638
pishtaz <pishtaz-ml@proton.me>
2026-02-06 19:37:11 +0100

    Fix: point freeze.py to custom static freezer to resolve Frozen-Flask charset error

--- freeze.py
+++ freeze.py
@@ -1,18 +1,4 @@
-from flask_frozen import Freezer
-from app import app, get_categories, get_articles_in_category
-
-freezer = Freezer(app)
-
-@freezer.register_generator
-def category_page():
-    for cat in get_categories():
-        yield {'category': cat}
-
-@freezer.register_generator
-def article_page():
-    for cat in get_categories():
-        for art in get_articles_in_category(cat):
-            yield {'category': cat, 'slug': art['slug']}
+from freeze_static import main
 
 if __name__ == '__main__':
-    freezer.freeze()
+    main()
